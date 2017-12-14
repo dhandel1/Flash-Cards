f2cb346dfcca52ee3d0de496305ed9f8eeb1edbc
David Handel <dhandel1@hotmail.com>
2017-12-13 20:16:50 -0500

    added script to move to next card

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 //contains code to flip the card
 
-$("button").click(function(){
+$("#flip").click(function(){
     $("#card").toggleClass("flipped");
+});
+
+//this is the array of images
+
+// var imgNames = ('images/picard.jpg', 'images/worf.jpg', 'images/spock.jpg');
+
+//this is code to move to next card when marked correct
+
+$('#correct').click(function() {
+    $('#card .front').each(function( index, element ) {
+      $('#card .front').css('background', 'url(spock.jpg)');  
+      $('#card .back').html('Its Spock!');  
+    });
+});
+
+//this is code to mark incorrect, leave in array and move to next card
+
+
+$('#wrong').click(function() {
+    $('#card .front').each(function( index, element ) {
+      $('#card .front').css('background', 'url(spock.jpg)');  
+      $('#card .back').html('Its Spock!');  
+    });
 });
